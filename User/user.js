39c3d7c0da6459d52b430235c37be4841e0966bb
RushiArlekar <48595683+RushiArlekar@users.noreply.github.com>
2020-04-06 05:57:48 +0530

    Add files via upload

--- User/user.js
+++ User/user.js
@@ -1,13 +1,46 @@
 
 function appendphotos(data){
   var maindiv = document.getElementById("gallerydiv");
-  
+  //maindiv.innerHTML=data;
+
+  jsongallery = JSON.parse(data);
+    
+    for (var i = 0; i < jsongallery.length; i++) {
+
+      var gdiv = document.createElement('IMG');
+      gdiv.id = 'gdiv';
+      gdiv.setAttribute("width","320px");
+      gdiv.setAttribute("height","180px");
+      gdiv.setAttribute("src",fetch('https://jsonplaceholder.typicode.com/photos?photoId='+jsongallery[i].photoId+'&id='+jsongallery[i].id)
+      .then(response => response.json())
+      .then(json => {
+        //console.log(json)
+
+        photo = JSON.parse(json);
+        console.log(photo);
+        //maindiv.appendChild(gdiv);
+        var photos = `
+        <iframe width="180" height="90" 
+        src="${photo[i].thumbnailUrl}?frameborder=0">
+        </iframe>
+        
+        `
+        gdiv.appendChild(photos);
+      }));
+
+      //div.appendChild(document.create);
+      //gdiv.innerHTML=jsongallery[i].url;
+      
+      maindiv.appendChild(gdiv);
+  }
+
 }
 
 function displaygellery(id){
-  console.log("yes "+id);
+  //console.log("yes "+id);
   get('https://jsonplaceholder.typicode.com/photos?albumId='+id).then(function(response){
     console.log("success",response);
+    //console.log(id);
     appendphotos(response);
   },function(error){
     console.log("failed",error);
@@ -16,22 +49,24 @@ function displaygellery(id){
 
 function appendData(data) {
   var listdiv = document.getElementById("userdiv");
-
-
-  for (var i = 0; i < data.length; i++) {
-    var div = document.createElement('div');
-    div.id = 'userrec';
-    div.className='rec';
+    
     jsondata = JSON.parse(data);
     
-    div.innerHTML = jsondata[i].name;
-    //div.innerHTML = data;
-    listdiv.appendChild(div);
-    div.style.status="";
-    
-    div.addEventListener("click",displaygellery(jsondata[i].id));
+    for (var i = 0; i < jsondata.length; i++) {
 
+      var div = document.createElement('div');
+      div.id = 'udiv';
+      div.className=jsondata[i].id;
+      
+      div.setAttribute("type","button");
+      //div.appendChild(document.createTextNode(jsondata[i].name)); 
+      div.innerHTML=jsondata[i].name;
+      
+      listdiv.appendChild(div);
   }
+  var usr = document.getElementById('udiv');
+  //usr.addEventListener("onClick",displaygellery(usr.className));
+  usr.onclick = displaygellery(usr.className);
 }
 
 function get(url) {
